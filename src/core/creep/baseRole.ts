0b50ee329cf85539a15be3898fd55d327829342c
Resi Respati <resir014@gmail.com>
2017-03-28 17:04:55 +0700

    refactor: moved BaseCreep inside the creep directory

--- src/core/creep/baseRole.ts
+++ src/core/creep/baseRole.ts
@@ -1,4 +1,4 @@
-import BaseCreep from "../creep";
+import BaseCreep from "./baseCreep";
 
 /**
  * Shared roles for all Creeps.
@@ -17,5 +17,49 @@ export default class BaseRole extends BaseCreep {
    */
   constructor(creep: Creep) {
     super(creep);
+  }
+
+  /**
+   * Extended method of `Creep.moveTo()`.
+   *
+   * @param {(RoomPosition | { pos: RoomPosition })} target
+   * @param {number} maxRooms
+   * @returns {number}
+   */
+  public moveTo(target: Structure | RoomPosition, maxRooms: number): number {
+    let self = this;
+    let result: number = 0;
+
+    // Execute moves by cached paths at first
+    result = self.creep.moveTo(target, { noPathFinding: true });
+
+    // Perform pathfinding only if we have enough CPU
+    if (result !== 0) {
+      if (Game.cpu.tickLimit - Game.cpu.getUsed() > 20) {
+        result = self.creep.moveTo(target, { maxRooms: maxRooms });
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Shorthand method for `renewCreep()`.
+   *
+   * @param {Spawn} spawn
+   * @returns {number}
+   */
+  public tryRenew(spawn: Spawn): number {
+    return spawn.renewCreep(this.creep);
+  }
+
+  /**
+   * Moves a creep to a designated renew spot (in this case the spawn).
+   *
+   * @param {Spawn} spawn
+   */
+  public moveToRenew(spawn: Spawn): void {
+    if (this.tryRenew(spawn) === ERR_NOT_IN_RANGE) {
+      this.creep.moveTo(spawn);
+    }
   }
 }

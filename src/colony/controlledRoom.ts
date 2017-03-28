dc33a9217e266608d9f7d78236a0e1863d3fa556
Resi Respati <resir014@gmail.com>
2017-03-28 17:08:26 +0700

    refactor(ControlledRoomColony): moved CreepBuilder into its own method

--- src/colony/controlledRoom.ts
+++ src/colony/controlledRoom.ts
@@ -12,15 +12,19 @@ export default class ControlledRoomColony extends Colony {
     let spawn: Spawn = this.room.find<Spawn>(FIND_MY_SPAWNS)[0];
     let creeps: Creep[] = this.room.find<Creep>(FIND_MY_CREEPS);
 
+    if (creeps.length < 1) {
+      this.buildCreep(spawn, "sourceMiner", [WORK, WORK, MOVE, CARRY]);
+    }
+  }
+
+  private buildCreep(spawn: Spawn, role: string, bodyParts: string[]) {
     let payload: IModulePayload = {
       spawn: spawn,
-      role: "sourceMiner",
+      role: role,
       room: spawn.room.name,
-      bodyParts: [WORK, WORK, MOVE, CARRY]
+      bodyParts: bodyParts
     };
 
-    if (creeps.length < 1) {
-      new CreepBuilder(MemoryManager.getGuid(), payload).runModule();
-    }
+    new CreepBuilder(MemoryManager.getGuid(), payload).runModule();
   }
 }

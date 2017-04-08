fc6bc017b2e3b72b2ea817266bf5323e1824098b
Resi Respati <resir014@gmail.com>
2017-04-08 23:47:04 +0700

    refactor: added new base + orchestrator class

---
+++ src/core/orchestrator.ts
@@ -0,0 +1,48 @@
+import { controlledRoomJobs } from "../config/jobs";
+import SourceManager from "../shared/sourceManager";
+
+/**
+ * Orchestrator is the brain of each Colony. It provides a facility to perform
+ * global managerial tasks within a Colony, including managing job assignment,
+ * job priorities, mining/construction positions, etc.
+ */
+export default class Orchestrator {
+  public room: Room;
+  public sourceManager: SourceManager;
+
+  private memory: { [key: string]: any };
+
+  /**
+   * Creates an instance of Orchestrator.
+   *
+   * @param room The current room.
+   */
+  constructor(room: Room) {
+    this.room = room;
+    this.memory = room.memory;
+    this.sourceManager = new SourceManager(room);
+  }
+
+  /**
+   * Refreshes the job assignment available in a room.
+   *
+   * @todo If `manualJobControl` is set to `false in the room memory, it's going
+   * to invoke a method which will ~automagically~ define job assignments based
+   * on some parameters. We don't even have that function yet.
+   */
+  public refreshJobAssignments() {
+    // Check if all job assignments are initialised properly.
+    let availableJobs = _.intersection(_.keys(this.memory.jobs), controlledRoomJobs);
+    if (!availableJobs) {
+      for (let i in controlledRoomJobs) {
+        this.memory.jobs[controlledRoomJobs[i]] = 0;
+      }
+    }
+    if (availableJobs.length !== controlledRoomJobs.length) {
+      let jobsToAdd = _.difference(_.keys(this.memory.jobs), controlledRoomJobs);
+      for (let i in jobsToAdd) {
+        this.memory.jobs[jobsToAdd[i]] = 0;
+      }
+    }
+  }
+}
---
+++ src/core/stonehenge.ts
@@ -0,0 +1,68 @@
+import { log } from "../lib/logger";
+import { profile } from "../lib/profiler";
+import * as MemoryManager from "../shared/memoryManager";
+import ColonyManager from "../colony/colonyManager";
+
+/**
+ * Stonehenge is a colony management system for the game Screeps designed with
+ * modularity in mind.
+ */
+export default class Stonehenge {
+  @profile
+  public run() {
+    // Check memory for null or out of bounds custom objects.
+    this.checkOutOfBoundsMemory();
+
+    // For each controlled room, run colony actions.
+    for (let i in Game.rooms) {
+      let room: Room = Game.rooms[i];
+
+      let colony = new ColonyManager(room);
+      colony.run();
+    }
+
+    log.info("Stonehenge.run()");
+  }
+
+  /**
+   * Creates a unique guid for a creep/queued task.
+   *
+   * @export
+   * @returns The guid used for a task
+   */
+  protected getGuid() {
+    if (!Memory.guid || Memory.guid > 100) {
+      Memory.guid = 0;
+    }
+
+    Memory.guid = Memory.guid + 1;
+    return Memory.guid;
+  }
+
+  /**
+   * Check memory for null or out of bounds custom objects
+   *
+   * @export
+   */
+  private checkOutOfBoundsMemory() {
+    if (!Memory.guid || Memory.guid > 100) {
+      Memory.guid = 0;
+    }
+
+    if (!Memory.creeps) {
+      Memory.creeps = {};
+    }
+    if (!Memory.flags) {
+      Memory.flags = {};
+    }
+    if (!Memory.rooms) {
+      Memory.rooms = {};
+    }
+    if (!Memory.spawns) {
+      Memory.spawns = {};
+    }
+    if (!Memory.modules) {
+      Memory.modules = {};
+    }
+  }
+}

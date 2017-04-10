64252d018af2ae46b0cfbe61b9a861ce8080a19b
Resi Respati <resir014@gmail.com>
2017-04-10 18:06:40 +0700

    refactor(Role): separate `moveTo()` method
    To move to an exact RoomPosition, use `moveToPosition()` instead.
    
    BREAKING CHANGE: `moveTo()` no longer accepts a `RoomPosition` property

--- src/colony/role/role.ts
+++ src/colony/role/role.ts
@@ -24,7 +24,30 @@ export default class Role {
    * @param {number} maxRooms
    * @returns {number}
    */
-  public moveTo(target: Structure | RoomPosition, maxRooms: number): number {
+  public moveTo<T extends RoomObject>(target: T, maxRooms: number): number {
+    let self = this;
+    let result: number = 0;
+
+    // Execute moves by cached paths at first
+    result = self.creep.moveTo(target, { noPathFinding: true });
+
+    // Perform pathfinding only if we have enough CPU
+    if (result !== 0) {
+      if (Game.cpu.tickLimit - Game.cpu.getUsed() > 20) {
+        result = self.creep.moveTo(target, { maxRooms: maxRooms });
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Extended method of `Creep.moveTo()`, adjusted for RoomPosition.
+   *
+   * @param {(RoomPosition | { pos: RoomPosition })} target
+   * @param {number} maxRooms
+   * @returns {number}
+   */
+  public moveToPosition(target: RoomPosition, maxRooms: number): number {
     let self = this;
     let result: number = 0;
 

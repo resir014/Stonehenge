21262144a428c888741df7d15743ddb1e1e593dd
Resi Respati <resir014@gmail.com>
2017-04-12 11:16:07 +0700

    feat(StructureManager): added some useful functions

--- src/shared/structureManager.ts
+++ src/shared/structureManager.ts
@@ -13,4 +13,64 @@ export default class StructureManager {
       log.debug("[StructureManager]" + this.structureCount + " structures found.");
     }
   }
+
+  /**
+   * Get the storage objects available. This prioritizes StructureContainer,
+   * but will fall back to an extension, or to the spawn if need be.
+   */
+  public getStorageObjects(): Structure[] {
+    let targets: Structure[] = this.structures.filter((structure: StructureContainer) => {
+      return ((structure.structureType === STRUCTURE_CONTAINER)
+        && _.sum(structure.store) < structure.storeCapacity);
+    });
+
+    // if we can't find any storage containers, use either the extension or spawn.
+    if (targets.length === 0) {
+      targets = this.structures.filter((structure: StructureExtension) => {
+        return ((structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
+          structure.energy < structure.energyCapacity);
+      });
+    }
+
+    return targets;
+  }
+
+  /**
+   * Get the energy dropoff points available. This prioritizes the spawn,
+   * falling back on extensions, then towers, and finally containers.
+   */
+  public getDropOffPoints(): Structure[] {
+    let targets: Structure[] = this.structures.filter((structure) => {
+      if (structure instanceof Spawn) {
+        return ((structure.structureType === STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity);
+      }
+    });
+
+    // If the spawn is full, we'll find any extensions/towers.
+    if (targets.length === 0) {
+      targets = this.structures.filter((structure) => {
+        if (structure instanceof StructureExtension) {
+          return ((structure.structureType === STRUCTURE_EXTENSION)
+            && structure.energy < structure.energyCapacity);
+        }
+      });
+    }
+
+    // Or if that's filled as well, look for towers.
+    if (targets.length === 0) {
+      targets = this.structures.filter((structure: StructureTower) => {
+        return ((structure.structureType === STRUCTURE_TOWER)
+          && structure.energy < structure.energyCapacity - (structure.energyCapacity * 0.5));
+      });
+    }
+
+    // Otherwise, look for storage containers.
+    if (targets.length === 0) {
+      targets = this.structures.filter((structure: StructureStorage) => {
+        return ((structure.structureType === STRUCTURE_STORAGE) && _.sum(structure.store) < structure.storeCapacity);
+      });
+    }
+
+    return targets;
+  }
 }

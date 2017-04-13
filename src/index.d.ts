83d8f2d1a7ec8d5fdac32185407162bf0ade297b
Resi Respati <resir014@gmail.com>
2017-04-13 10:51:37 +0700

    feat: enhanced type declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,38 @@
-interface Memory {
-  uuid: number;
-  log: any;
-}
-
 declare function require(path: string): any;
 
-interface Global {
+/**
+ * Extended memory objects.
+ */
+interface Memory {
+  creeps: { [key: string]: any };
+  flags: { [key: string]: any };
+  rooms: { [key: string]: any };
+  spawns: { [key: string]: any };
+  guid: number;
   log: any;
+  profiler: any;
+}
+
+/**
+ * Interface for the global objects.
+ */
+interface Global {
+  /**
+   * Tweak your Logger settings using this global.
+   */
+  log: {
+    level: number,
+    showSource: boolean,
+    showTick: boolean
+  };
+
+  /**
+   * Prints out a table of the global memory stats.
+   */
   printMemProfilerStats: any;
 }
 
+/**
+ * Global objects that can be called from the Screeps console.
+ */
 declare var global: Global;

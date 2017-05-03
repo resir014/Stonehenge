47931ee2995a42b36e24aac74644f36050da5376
Resi Respati <resir014@gmail.com>
2017-05-03 16:42:28 +0700

    style: added docblocks/removed useless declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,46 @@
 // BEGIN Extended prototypes
 
 interface Structure {
+  /**
+   * Checks if a certain structure needs to be refilled with energy.
+   *
+   * @returns {boolean} true if a certain structure needs to be refilled with energy.
+   *
+   * @memberof Structure
+   */
   needEnergy(): boolean
 }
 
 interface StructureSpawn {
+  /**
+   * Gets the largest buildable body parts from a certain set of available body parts.
+   *
+   * @param {string[][]} potentialBodies A list of potential body parts for the spawned creep.
+   * @returns {string[]} the largest buildable body part.
+   *
+   * @memberof StructureSpawn
+   */
   getLargestBuildableBodyFromSet(potentialBodies: string[][]): string[]
+  /**
+   * Gets the largest buildable body parts for a certain body part template.
+   *
+   * @param {string[]} bodyTemplate The target body template.
+   * @param {number} [maxIterations] Maximum iterations.
+   * @returns {string[]} the largest buildable body part.
+   *
+   * @memberof StructureSpawn
+   */
   getLargestBuildableBodyFromTemplate(bodyTemplate: string[], maxIterations?: number): string[]
+  /**
+   * Gets the optimal amount of `MOVE` parts for a certain body part.
+   *
+   * @param {string[]} body The body parts to be calulated (without `MOVE` parts).
+   * @param {('road' | 'plain' | 'swamp')} [terrain] The target terrain type for a creep.
+   * @param {boolean} [fullCarry] set to `true` if the spawned creep is carrying the max amount of items.
+   * @returns {number} the optimal count of `MOVE` parts.
+   *
+   * @memberof StructureSpawn
+   */
   findOptimalMoveCountForBody(body: string[], terrain?: 'road' | 'plain' | 'swamp', fullCarry?: boolean): number
 }
 
@@ -37,11 +71,6 @@ interface Global {
     showSource: boolean,
     showTick: boolean
   }
-
-  /**
-   * Prints out a table of the global memory stats.
-   */
-  printMemProfilerStats: any
 }
 
 declare function require(path: string): any
